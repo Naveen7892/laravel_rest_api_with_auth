d9afdab9722e750b6248badab077d1c6d24bab0d
Naveen Kumar <Naveen Kumar>
2019-01-10 15:56:36 +0530

    Adding columns in table

---
+++ database/migrations/2019_01_10_102118_adds_api_token_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddsApiTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Adds column in DB with the following type and constraints.
+            $table->string('api_token', 60)->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Drops the column
+            $table->dropColumn(['api_token']);
+        });
+    }
+}
