d3e4290bc9d2cce8359776b93b537aaa4130306d
Naveen Kumar <Naveen Kumar>
2019-01-10 15:35:38 +0530

    Added Seeds for model User and Article and seeded

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    // mass assigned
+    // Fields inside the $fillable property can be mass assigned using Eloquent’s create() and update() methods. You can also use the $guarded property, to allow all but a few properties.
+    protected $fillable = ['title', 'body'];
 }
